728174494570dbdac35580ecd9935dcddd857fb0
Yehui <yehui.zh@gmail.com>
2019-04-29 19:59:44 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hello, World. 你好，世界。
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
